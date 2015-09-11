2313a97c35068d7f2ec0129561dda6792b3d9eb4
neilshah22 <nnshah@NN123-HA-24.HS.sd203.org>
2015-09-11 13:27:30 -0500

    car

---
+++ chp 3 peac/Car.java
@@ -0,0 +1,42 @@
+
+
+/**
+ * A car has a certain fuel efficiency and a certain amount of fuel in the gass tank 
+ * The car may be driven which reduces the amount of gas in the fuel tank.
+ * 
+ * @author Neil Shah
+ * @version 10 September 2015
+ */
+public class Car
+{
+    /** the fuel efficiency for the car measured in units of miles/gallon (mpg) */
+    private double fuelEfficiency;
+    
+    /**the amount of fuel in the tank of the car measured in units of gallons*/
+    private double fuelInTank; 
+    
+    
+    /** 
+     * Default constructor for objects of class Car
+     */
+    public Car(double fuelEfficiency)
+    {
+        fuelEfficiency = fuelEfficiency
+        x = 0;
+    }
+
+    /**
+     * this method simulates driving the car for the specified distence and 
+     * reduces the amount of gas in the fuel tank
+     *
+     * @pre        
+     * @param    distence   the specified distence will not consume more than 
+     * @return    description of the return value
+     */
+    public void drive( double distance )
+    {
+        
+        
+    }
+
+}
