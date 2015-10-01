e6883f69c5d4a1769e42b5c69446e6183bdd0864
neilshah22 <nnshah@stu.naperville203.org>
2015-09-30 21:53:57 -0500

    done

--- chp 3 peac/Target.java
+++ chp 3 peac/Target.java
@@ -1,5 +1,4 @@
 import java.awt.Graphics2D;
-import java.awt.Rectangle;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
@@ -8,4 +7,23 @@ public class Target
 {
     private int xLeft;
     private int yTop;
+    public Target(int x, int y)
+    {
+        xLeft = x;
+        yTop = y;
+    }
+    
+    public void draw(Graphics2D g2)
+    {
+        Ellipse2D.Double layer1 = new Ellipse2D.Double(xLeft + 10, yTop + 20, 15, 15);
+        Ellipse2D.Double layer2 = new Ellipse2D.Double(xLeft + 10, yTop + 20, 10, 10);
+        Ellipse2D.Double layer3 = new Ellipse2D.Double(xLeft + 10, yTop + 20, 5, 5);
+        g2.draw(layer1);
+        g2.draw(layer2);
+        g2.draw(layer3);
+        g2.fill(layer1);
+        g2.fill(layer2);
+        g2.fill(layer3);
+    }
+}
     
